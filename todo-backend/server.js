5d1626ce69f8cb023b0fae3dc065952d221da67f
AFFAN AHMED <affanahmed0128@gmail.com>
2026-02-13 16:14:56 +0530

    Basic structure functions added 2

--- todo-backend/server.js
+++ todo-backend/server.js
@@ -5,14 +5,13 @@ const mongoose = require("mongoose")
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/TodoApp")
+    await mongoose.connect("mongodb://127.0.0.1:27017/todoapp")
     console.log("mongodb connected (local)")
   } catch (error) {
     console.error("Mongo db connection failed", error)
   }
 }
  
-module.exports = connectDB;
 
 const app = express();
 app.use(cors())
@@ -21,10 +20,31 @@ app.use(express.json())
 const Todo = require("./models/todoModel")
 
 
+app.get('/get-todo', async(req,res) =>{
+    console.log("Fetching the todos from DB")
+    try{
+       const todos=await Todo.find();
+       console.log("fetched all the todos",todos)
+       res.status(200).json(todos)
+    }catch(error){
+       console.log("Error while fetching the todos",error)
+       res.status(500).json({message:"something wet wrong,please try later"})
+    }
+})
+
 app.post('/add-todo', async(req,res) =>{
-    const {title} = req.body;
-    console.log("Adding a new todo",title)
-    res.status(200).json({message:"Hello Im BATMAN , DO U BLEED!"})
+    const title = req.body;
+    // console.log("Adding a new todo",req.body)
+    console.log("Adding a new todo",title.todo)
+    const newTodo=new Todo({
+      title:title.todo 
+    })
+    console.log("Adding the todo to DB",newTodo)
+    const savedTodo=await newTodo.save()
+    console.log("Adding the todo to DB",savedTodo)
+
+
+    res.status(200).json(savedTodo)
 })
 
 connectDB()
