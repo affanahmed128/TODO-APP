c9a3e8ace76b4d4a30a93bef74c873c14fe1c1a1
AFFAN AHMED <affanahmed0128@gmail.com>
2026-02-15 12:15:14 +0530

    unit test done

--- todo-backend/__tests__/controllers/todoController.test.js
+++ todo-backend/__tests__/controllers/todoController.test.js
@@ -1,6 +1,34 @@
 const todoController=require("../../contollers/todoController")
+
+jest.mock("../../models/todoModel.js")
+const mockSave=jest.fn();
+const mockFind=jest.fn();
+
+const Todo=require("../../models/todoModel")
+Todo.find=mockFind
+
 describe("Todo Controller is invoked",() =>{
+    let req,res;   
+    beforeEach(()=>{
+        req={
+            body:{},
+            params:{}
+        };
+        res={
+            json:jest.fn(()=>res),
+            status:jest.fn(()=>res),
+        }
+    }) 
+
     describe("For getTodos function ", ()=>{
-        expect(true).tobe(true)
+        it("should return me all todos",async ()=>{
+            const mockTodos= [{_id: 0,title:"Todo 1",completed: false},{_id: 1,title:"Todo 1",completed: false},{_id: 1,title:"Todo 2",completed: false},{_id: 2,title:"Todo 3",completed: false}]
+            mockFind.mockResolvedValue(mockTodos)
+            await todoController.getTodos(req,res);
+
+            expect(mockFind).toHaveBeenCalled()
+            expect(res.status).toHaveBeenCalledWith(200)
+            expect(res.json).toHaveBeenCalledWith(mockTodos)
+        })
     })
 })
